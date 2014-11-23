a7d733a9d5baf1690b513dfc3b3a03ff2dcc3341
Stavros Zavrakas <stavros.zavrakas@styloola.com>
2014-11-23 09:22:06 +0000

    splitting the views

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -1,53 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport"    content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author"      content="Sergey Pozhilov (GetTemplate.com)">
-    
-    <title>Welcome to loanbook</title>
+@extends('layouts.master')
 
-    <link rel="shortcut icon" href="assets/images/gt_favicon.png">
-    
-    <link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
-    <link rel="stylesheet" href="//cdn.datatables.net/plug-ins/9dcbecd42ad/integration/bootstrap/3/dataTables.bootstrap.css">
-
-    <!-- Custom styles for our template -->
-    <link rel="stylesheet" href="assets/css/bootstrap-theme.css" media="screen" >
-    <link rel="stylesheet" href="assets/css/main.css">
-    <link rel="stylesheet" href="assets/css/override.css">
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-    <script src="assets/js/html5shiv.js"></script>
-    <script src="assets/js/respond.min.js"></script>
-    <![endif]-->
-</head>
-
-<body class="home">
-    <!-- Fixed navbar -->
-    <div class="navbar navbar-inverse navbar-fixed-top headroom" >
-        <div class="container">
-            <div class="navbar-header">
-                <!-- Button for smallest screens -->
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-                <a class="navbar-brand" href="{{ URL::action('HomeController@index') }}"><img src="assets/images/logo.png" alt="Progressus HTML5 template"></a>
-            </div>
-            <div class="navbar-collapse collapse">
-                <ul class="nav navbar-nav pull-right">
-                    <li class="active"><a href="{{ URL::action('HomeController@index') }}">Home</a></li>
-                    <li><a href="{{ URL::action('HomeController@index') }}">About</a></li>
-                    <li><a href="{{ URL::action('HomeController@index') }}">Contact</a></li>
-                    <li><a class="btn" href="{{ URL::action('AuthController@logout') }}">LOGOUT</a></li>
-                </ul>
-            </div><!--/.nav-collapse -->
-        </div>
-    </div> 
-    <!-- /.navbar -->
-
+@section('body')
     <!-- Dummy header -->
     <div class="dummy-header"></div>
     <!-- /Dummy header -->
@@ -56,13 +9,13 @@
     <div class="container">
       <div class="row">
       	<div class="col-md-12 col-sm-12 highlight">
-	    		<h1>Your suggested portfolio</h1>
-          <p class="text-muted">Give a short explanation of the portfolio</p>
-          @include('includes.your_portfolio')
-          <h2>Recommended interests</h2>
-          <p class="text-muted">Give a short explanation of the recommended interests</p>
-          <div id="recommended_interests"></div>
-	    	</div>
+            <h1>Your suggested portfolio</h1>
+            <p class="text-muted">Give a short explanation of the portfolio</p>
+            @include('includes.your_portfolio')
+            <h2>Recommended interests</h2>
+            <p class="text-muted">Give a short explanation of the recommended interests</p>
+            <div id="recommended_interests"></div>
+        	</div>
     	</div>
     </div>
     <!-- /Intro-->
@@ -102,106 +55,5 @@
         
         </div>
     </div>
-    
-    <!-- Social links. @TODO: replace by link/instructions in template -->
-    <section id="social">
-        <div class="container">
-            <div class="wrapper clearfix">
-                <!-- AddThis Button BEGIN -->
-                <div class="addthis_toolbox addthis_default_style">
-                <a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>
-                <a class="addthis_button_tweet"></a>
-                <a class="addthis_button_linkedin_counter"></a>
-                <a class="addthis_button_google_plusone" g:plusone:size="medium"></a>
-                </div>
-                <!-- AddThis Button END -->
-            </div>
-        </div>
-    </section>
-    <!-- /social links -->
 
-    <footer id="footer" class="top-space">
-
-        <div class="footer1">
-            <div class="container">
-                <div class="row">
-                    
-                    <div class="col-md-3 widget">
-                        <h3 class="widget-title">Contact</h3>
-                        <div class="widget-body">
-                            <p>+234 23 9873237<br>
-                                <a href="mailto:#">some.email@somewhere.com</a><br>
-                                <br>
-                                234 Hidden Pond Road, Ashland City, TN 37015
-                            </p>    
-                        </div>
-                    </div>
-
-                    <div class="col-md-3 widget">
-                        <h3 class="widget-title">Follow me</h3>
-                        <div class="widget-body">
-                            <p class="follow-me-icons">
-                                <a href=""><i class="fa fa-twitter fa-2"></i></a>
-                                <a href=""><i class="fa fa-dribbble fa-2"></i></a>
-                                <a href=""><i class="fa fa-github fa-2"></i></a>
-                                <a href=""><i class="fa fa-facebook fa-2"></i></a>
-                            </p>    
-                        </div>
-                    </div>
-
-                    <div class="col-md-6 widget">
-                        <h3 class="widget-title">Text widget</h3>
-                        <div class="widget-body">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, dolores, quibusdam architecto voluptatem amet fugiat nesciunt placeat provident cumque accusamus itaque voluptate modi quidem dolore optio velit hic iusto vero praesentium repellat commodi ad id expedita cupiditate repellendus possimus unde?</p>
-                            <p>Eius consequatur nihil quibusdam! Laborum, rerum, quis, inventore ipsa autem repellat provident assumenda labore soluta minima alias temporibus facere distinctio quas adipisci nam sunt explicabo officia tenetur at ea quos doloribus dolorum voluptate reprehenderit architecto sint libero illo et hic.</p>
-                        </div>
-                    </div>
-
-                </div> <!-- /row of widgets -->
-            </div>
-        </div>
-
-        <div class="footer2">
-            <div class="container">
-                <div class="row">
-                    
-                    <div class="col-md-6 widget">
-                        <div class="widget-body">
-                            <p class="simplenav">
-                                <a href="#">Home</a> | 
-                                <a href="about.html">About</a> |
-                                <a href="sidebar-right.html">Sidebar</a> |
-                                <a href="contact.html">Contact</a> |
-                                <b><a href="signup.html">Sign up</a></b>
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="col-md-6 widget">
-                        <div class="widget-body">
-                            <p class="text-right">
-                                Copyright &copy; 2014, Your name. Designed by <a href="http://gettemplate.com/" rel="designer">gettemplate</a> 
-                            </p>
-                        </div>
-                    </div>
-
-                </div> <!-- /row of widgets -->
-            </div>
-        </div>
-
-    </footer>
-
-    <!-- JavaScript libs are placed at the end of the document so the pages load faster -->
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-    <script src="assets/js/headroom.min.js"></script>
-    <script src="assets/js/jQuery.headroom.min.js"></script>
-    <script src="assets/js/template.js"></script>
-    <script src="assets/flot/jquery.flot.js"></script>
-    <script src="assets/flot/jquery.flot.pie.js"></script>
-    <script src="//cdn.datatables.net/1.10.4/js/jquery.dataTables.min.js"></script>
-    <script src="//cdn.datatables.net/plug-ins/9dcbecd42ad/integration/bootstrap/3/dataTables.bootstrap.js"></script>
-    <script src="js/basic.js"></script>
-
-</body>
-</html>
+@stop
