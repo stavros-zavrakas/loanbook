e9d8d4d430e5bde93c30c8a34cce8c0da847d26f
Stavros Zavrakas <stavros.zavrakas@styloola.com>
2014-11-23 15:11:59 +0000

    fixes

--- app/views/includes/equity.blade.php
+++ app/views/includes/equity.blade.php
@@ -12,70 +12,70 @@
     </thead>
 
     <tbody>
-        <tr data-toggle="tooltip" data-placement="bottom" data-container="body" title="<img src='assets/images/connection.png' alt='Connection'>">
+        <tr data-toggle="tooltip" data-placement="bottom" data-container="body" title="<img src='assets/images/connection.jpg' alt='Connection'>">
             <td><img src='assets/static_images/bidu.jpeg' width='150'/></td>
             <td>Bidu</td>
             <td>6%</td>
             <td>£45.000</td>
             <td></td>
         </tr>
-        <tr data-toggle="tooltip" data-placement="bottom" data-container="body" title="<img src='assets/images/connection.png' alt='Connection'>">
+        <tr data-toggle="tooltip" data-placement="bottom" data-container="body" title="<img src='assets/images/connection.jpg' alt='Connection'>">
             <td><img src='assets/static_images/comqi.jpg' width='150'/></td>
             <td>ComQi</td>
             <td>6.8%</td>
             <td>£46.000</td>
             <td></td>
         </tr>
-        <tr data-toggle="tooltip" data-placement="bottom" data-container="body" title="<img src='assets/images/connection.png' alt='Connection'>">
+        <tr data-toggle="tooltip" data-placement="bottom" data-container="body" title="<img src='assets/images/connection.jpg' alt='Connection'>">
             <td><img src='assets/static_images/edgeware.jpeg' width='150'/></td>
             <td>Edgeware</td>
             <td>15.3%</td>
             <td>£340.000</td>
             <td></td>
         </tr>
-        <tr data-toggle="tooltip" data-placement="bottom" data-container="body" title="<img src='assets/images/connection.png' alt='Connection'>">
+        <tr data-toggle="tooltip" data-placement="bottom" data-container="body" title="<img src='assets/images/connection.jpg' alt='Connection'>">
             <td><img src='assets/static_images/aihit.jpeg' width='150'/></td>
             <td>Kelly</td>
             <td>11.3%</td>
             <td>£120.000</td>
             <td></td>
         </tr>
-        <tr data-toggle="tooltip" data-placement="bottom" data-container="body" title="<img src='assets/images/connection.png' alt='Connection'>">
+        <tr data-toggle="tooltip" data-placement="bottom" data-container="body" title="<img src='assets/images/connection.jpg' alt='Connection'>">
             <td><img src='assets/static_images/glysure.jpg' width='150'/></td>
             <td>Glysure</td>
             <td>17.4%</td>
             <td>£520.000</td>
             <td></td>
         </tr>
-        <tr data-toggle="tooltip" data-placement="bottom" data-container="body" title="<img src='assets/images/connection.png' alt='Connection'>">
+        <tr data-toggle="tooltip" data-placement="bottom" data-container="body" title="<img src='assets/images/connection.jpg' alt='Connection'>">
             <td><img src='assets/static_images/mydeco.jpg' width='150'/></td>
             <td>Mydeco</td>
             <td>8.8%</td>
             <td>£320.000</td>
             <td></td>
         </tr>
-        <tr data-toggle="tooltip" data-placement="bottom" data-container="body" title="<img src='assets/images/connection.png' alt='Connection'>">
+        <tr data-toggle="tooltip" data-placement="bottom" data-container="body" title="<img src='assets/images/connection.jpg' alt='Connection'>">
             <td><img src='assets/static_images/plastic_logic.jpg' width='150'/></td>
             <td>Plastic Logic</td>
             <td>10.1%</td>
             <td>£123.000</td>
             <td></td>
         </tr>
-        <tr data-toggle="tooltip" data-placement="bottom" data-container="body" title="<img src='assets/images/connection.png' alt='Connection'>">
+        <tr data-toggle="tooltip" data-placement="bottom" data-container="body" title="<img src='assets/images/connection.jpg' alt='Connection'>">
             <td><img src='assets/static_images/seven.jpeg' width='150'/></td>
             <td>Seven</td>
             <td>10.4%</td>
             <td>£176.000</td>
             <td></td>
         </tr>
-        <tr data-toggle="tooltip" data-placement="bottom" data-container="body" title="<img src='assets/images/connection.png' alt='Connection'>">
+        <tr data-toggle="tooltip" data-placement="bottom" data-container="body" title="<img src='assets/images/connection.jpg' alt='Connection'>">
             <td><img src='assets/static_images/tobii.jpg' width='150'/></td>
             <td>Tobii</td>
             <td>8.5%</td>
             <td>£30.000</td>
             <td></td>
         </tr>
-        <tr data-toggle="tooltip" data-placement="bottom" data-container="body" title="<img src='assets/images/connection.png' alt='Connection'>">
+        <tr data-toggle="tooltip" data-placement="bottom" data-container="body" title="<img src='assets/images/connection.jpg' alt='Connection'>">
             <td><img src='assets/static_images/vocaliq.jpg' width='150'/></td>
             <td>Vocaliq</td>
             <td>9%</td>
